bd8fdff88eaeae4dcb4d83f961c29f491fc8d2fe
Yash <aminyash8@gmail.com>
2018-09-28 21:37:31 +0530

    Changes

--- app/src/main/java/com/example/sanketpatel/translator/ViewProductActivity.java
+++ app/src/main/java/com/example/sanketpatel/translator/ViewProductActivity.java
@@ -1,18 +1,29 @@
 package com.example.sanketpatel.translator;
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
+
+import com.example.sanketpatel.translator.Utils.ViewUtils;
 
 import java.util.Objects;
 
 public class ViewProductActivity extends AppCompatActivity {
     Product product;
     ViewPager viewPager;
+    Button showImageButton, showTextButton;
+    TextView textView;
+    ImageView imageView;
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,18 +40,40 @@ public class ViewProductActivity extends AppCompatActivity {
         } catch (Exception e) {
         }
 
-        TextView textView = this.findViewById(R.id.viewProduct_text);
-        ImageView imageView = findViewById(R.id.viewProduct_imageView);
+        textView = findViewById(R.id.viewProduct_text);
+        imageView = findViewById(R.id.viewProduct_imageView);
 
         try {
             imageView.setImageURI(Uri.parse(product.getUri()));
         } catch (Exception e) {
         }
 
+        scrollView = findViewById(R.id.viewProduct_scrollView);
         textView.setText(product.getQuantity());
+        showImageButton = findViewById(R.id.viewProduct_showImageButton);
+        showTextButton = findViewById(R.id.viewProduct_showTextButton);
+        showTextButton.setTextColor(Color.GRAY);
 
         //init();
 
+    }
+
+    public void onShowTextButtonClick(View view) {
+        ViewUtils.setVisibilityGone(imageView);
+        ViewUtils.setVisible(scrollView);
+        ViewUtils.setEnable(showImageButton);
+        ViewUtils.setDisable(view);
+        showImageButton.setTextColor(Color.GRAY);
+        ((Button)view).setTextColor(Color.WHITE);
+    }
+
+    public void onShowImageButtonClick(View view) {
+        ViewUtils.setVisibilityGone(scrollView);
+        ViewUtils.setVisible(imageView);
+        ViewUtils.setEnable(showTextButton);
+        ViewUtils.setDisable(view);
+        showTextButton.setTextColor(Color.GRAY);
+        ((Button)view).setTextColor(Color.WHITE);
     }
 
     // Swipe layout
